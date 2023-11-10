5ca97a5192ae9569adc49b2fd2361695b24d736a
Abdul Fatahillah <abdulfatahillah17@gmail.com>
2023-11-10 19:47:13 +0700

    pertemuan 3

--- praktikum03/animal.php
+++ praktikum03/animal.php
@@ -4,59 +4,68 @@
 class Animal
 {
     # property animals
-    private $animals;
+    public $animals;
 
     # method constructor - mengisi data awal
     # parameter: data hewan (array)
     public function __construct($data)
     {
+        # Menginisialisasi properti $animals dgn data yang diberikan saat buat objek.
         $this->animals = $data;
     }
 
     # method index - menampilkan data animals
     public function index()
     {
-        foreach ($this->animals as $index => $animal) {
-            echo "$index. $animal <br>";
+        # Gunakan foreach untuk menampilkan data animals (array) satu per satu.
+        foreach ($this->animals as $animal) {
+            echo "$animal <br>";
         }
+
+        return $this; # Kembalikan objek saat selesai
     }
 
     # method store - menambahkan hewan baru
     # parameter: hewan baru
     public function store($data)
     {
+        # Method array_push untuk menambahkan data baru ke dalam array $animals.
         array_push($this->animals, $data);
+
+        return $this; # Kembalikan objek saat selesai
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
     public function update($index, $data)
     {
-        if (isset($this->animals[$index])) {
-            $this->animals[$index] = $data;
-        }
+        # Mengganti data di indeks tertentu dengan data baru.
+        $this->animals[$index] = $data;
+
+        return $this; # Kembalikan objek saat selesai
     }
 
     # method delete - menghapus hewan
     # parameter: index
     public function destroy($index)
     {
-        if (isset($this->animals[$index])) {
-            unset($this->animals[$index]);
-        }
+        # Menghapus data di indeks tertentu menggunakan unset.
+        unset($this->animals[$index]);
+
+        return $this; # Kembalikan objek saat selesai
     }
 }
 
 # membuat object
 # kirimkan data hewan (array) ke constructor
-$animal = new Animal(['Singa', 'Harimau', 'Gajah']);
+$animal = new Animal(["Sapi", "Kucing", "Panda"]);
 
 echo "Index - Menampilkan seluruh hewan <br>";
 $animal->index();
 echo "<br>";
 
 echo "Store - Menambahkan hewan baru <br>";
-$animal->store('Burung');
+$animal->store('burung');
 $animal->index();
 echo "<br>";
 
