813b292de66ebc795cbd49f0c382e3788bd42f83
Abdul Fatahillah <abdulfatahillah17@gmail.com>
2023-12-15 21:49:51 +0700

    recommit pertemuan 10

---
+++ praktikum10/synchronous.js
@@ -0,0 +1,9 @@
+console.log("Edo Membuka Bowser Google Chrome.");
+
+console.log(`
+Abdul download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Abdul membuka Youtube.");
